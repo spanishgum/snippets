0b3c54f19269508d85807d426603bd4fb6b97ddf
root <root@ipa1.spanishgum.com>
2019-12-05 20:32:53 -0700

    abstract

---
+++ mutex_exception_safety/README.md
@@ -0,0 +1,6 @@
+# Build and run
+
+```bash
+g++ -std=c++11 main.cpp -pthread
+./a.out
+```
--- mutex_exception_safety/main.cpp
+++ mutex_exception_safety/main.cpp
@@ -1,53 +1,38 @@
+#include <string>
 #include <thread>
 #include <mutex>
 #include <chrono>
 #include <exception>
 #include <iostream>
 
-void sleep() {
+void wait(std::string s) {
+	std::cout << "Thread " << s << std::endl << std::flush;
 	std::this_thread::sleep_for(std::chrono::milliseconds(1000));
 }
 
-void speak(std::string s) {
-	std::cout << "Thread " << s << std::endl << std::flush;
+void safe(std::mutex &m) {
+	std::lock_guard<std::mutex> lock(m);
+	throw std::exception();
 }
 
-void safe() {
-	std::mutex m;
-	auto lambda = [&m]() {
-		std::lock_guard<std::mutex> lock(m);
-		throw std::exception();
-	};
-	while (true) {
-		speak("safe");
-		try {
-			lambda();
-		} catch (const std::exception &e) {}
-		sleep();
-	}
+void unsafe(std::mutex &m) {
+	m.lock();
+	throw std::exception();
+	m.unlock();
 }
 
-void unsafe() {
+void run(decltype(safe) f, std::string id) {
 	std::mutex m;
-	auto lambda = [&m]() {
-		m.lock();
-		throw std::exception();
-		m.unlock();
-	};
 	while (true) {
-		speak("unsafe");
+		wait(id);
 		try {
-			lambda();
+			f(m);
 		} catch (const std::exception &e) {}
-		sleep();
 	}
 }
 
 int main() {
-	std::thread a(safe);
-	std::thread b(unsafe);
-	while (true) {
-		speak("main");
-		sleep();
-	}
+	std::thread a(run, safe, std::string("safe"));
+	std::thread b(run, unsafe, std::string("unsafe"));
+	while (true) wait("main");
 }
